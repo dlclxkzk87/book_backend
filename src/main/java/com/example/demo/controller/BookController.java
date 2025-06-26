f5c20285e7eb0224fd4ab8c779fddc231033d814
SeChang <ricril1033@gmail.com>
2025-06-26 17:04:15 +0900

    insert modify

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class BookController {
 	
 	// 등록
 	@PostMapping
-	public String save(Book b) {
+	public String save(@RequestBody Book b) {
 		dao.save(b);
 		return "OK" ;
 	}
