2b2a24f187e96b8d2d9e5782af5110b1267e5eb3
SeChang <ricril1033@gmail.com>
2025-06-26 16:10:44 +0900

    insert create

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,12 @@ public class BookController {
 	@GetMapping
 	public List<Book> list(){
 		return dao.findAll();
+	}
+	
+	// 등록
+	@PostMapping
+	public String save(Book b) {
+		dao.save(b);
+		return "OK" ;
 	}
 }
